d0707452bac7728ac5826cbb7a5cb86c86c1ca34
Yuri Albuquerque <yuridenommus@gmail.com>
2019-07-16 18:13:04 -0300

    The cflags in the build script are no longer necessary.

--- pp_full/build.rs
+++ pp_full/build.rs
@@ -36,11 +36,7 @@ fn compile_cmake() {
 	let mut make = cmake::Config::new("lib");
 
 	if cfg!(feature = "cuda") {
-		make.define("ETHASHCUDA", "ON")
-			.cflag("-I/opt/cuda/include")
-			.cflag("-I/usr/local/cuda/include")
-			.cxxflag("-I/usr/local/cuda/include")
-			.cxxflag("-I/opt/cuda/include");
+		make.define("ETHASHCUDA", "ON");
 	} else {
 		make.define("ETHASHCUDA", "OFF");
 	}
