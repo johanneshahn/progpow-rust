f9a2652d0df86654790abfe518fd05e3f5e0ea67
Johannes Hahn <j.hahn@eccence.de>
2024-12-27 18:32:20 +0100

    update hunter and boost

--- pp_full/lib/cmake/Hunter/config.cmake
+++ pp_full/lib/cmake/Hunter/config.cmake
@@ -1,8 +1,3 @@
-hunter_config(CURL VERSION ${HUNTER_CURL_VERSION} CMAKE_ARGS HTTP_ONLY=ON CMAKE_USE_OPENSSL=OFF CMAKE_USE_LIBSSH2=OFF CURL_CA_PATH=none)
+hunter_config(CURL VERSION ${HUNTER_CURL_VERSION} CMAKE_ARGS HTTP_ONLY=ON CMAKE_USE_OPENSSL=OFF CMAKE_USE_LIBSSH2=OFF)
 hunter_config(libjson-rpc-cpp VERSION ${HUNTER_libjson-rpc-cpp_VERSION} CMAKE_ARGS TCP_SOCKET_SERVER=ON)
-hunter_config(
-    Boost
-    VERSION 1.80.0_new_url
-    SHA1 4ff012adaa7d8952913be05bf4233005de48289c
-    URL https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz
-)
+hunter_config(Boost VERSION 1.70.0-p0)
